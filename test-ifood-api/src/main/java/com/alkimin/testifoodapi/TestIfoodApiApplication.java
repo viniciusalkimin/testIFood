1101d4d6352f862ce9a37252d5157158196fca44
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2024-01-15 12:11:23 -0300

    Criando projeto base do api

---
+++ test-ifood-api/src/main/java/com/alkimin/testifoodapi/TestIfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.alkimin.testifoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIfoodApiApplication.class, args);
+	}
+
+}
---
+++ test-ifood-api/src/test/java/com/alkimin/testifoodapi/TestIfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.testifoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestIfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
